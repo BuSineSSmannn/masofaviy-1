a882f3f52c354b38d3c5bc69f9d71b1ae5b95a07
Jayson Branch <Jaysonbranch@mail.ru>
2020-11-17 20:34:24 +0500

    5-oy 1-dars

---
+++ 5-oy/1-dars/index.php
@@ -0,0 +1,88 @@
+
+<hr>
+<?php
+
+//Server qanday ishlaydi Server nima PHP nima Opserverni o'rnatish sozlash PHP o'zgaruvchilar
+
+
+//int string array bool obj float
+
+$age = 21;
+$name = 'Javoxir';
+$ish = "Dev";
+const g = 10;
+DEFINE('Gi',100);
+
+echo $age;
+echo "<br>";
+echo $name;
+echo "<br>";
+echo $ish;
+echo "<br>";
+echo 30;
+echo "<br>";
+echo 'This is string';
+echo '<br>';
+echo "Nimadir....";
+echo '<br>';
+echo $name ." - ". $age . " yosh";
+echo '<br>';
+
+echo "Salom birna $name";
+echo '<br>';
+echo 'O\'gil bola';
+echo '<br>';
+echo " Bib narsa \"test\" asdasd";
+echo '<br>';
+echo " O'gil bola";
+
+$name = "Javoxirbek";
+echo '<br>';
+$NAME = 'asd';
+
+echo g;
+echo Gi;
+echo '<br>';
+DEFINE('Gi',1000);
+const g = 5;
+echo '<br>';
+echo g . ' - g';
+
+echo Gi . ' - Gi';
+echo '<br>';
+
+
+$n = 'Chtoto ne tak...';
+$t = 'n';
+
+echo $name;
+echo '<br>';
+
+echo $$t;
+
+$var1 = 'string';
+$var2 = &$var1;
+echo '<br>';
+
+echo $var1;
+echo '<br>';
+echo $var2;
+echo '<hr>';
+$var1 = 'ewe string';
+echo $var1;
+echo '<br>';
+echo $var2;
+
+
+
+
+
+
+?>
+
+
+<b>asdasd</b>
+
+<h2><?=$$t?></h2>
+<script>alert(' <?php echo $name ?> ')</script>
+
